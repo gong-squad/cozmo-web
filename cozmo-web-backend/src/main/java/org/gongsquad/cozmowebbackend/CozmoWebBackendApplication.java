2463c7e8208d81b0e466cfa25ef6d18416cd852b
cgganaban <gecocarlganaban@gmail.com>
2022-09-10 01:57:11 +0800

    bootstrap backend and frontend

--- cozmo-web-backend/src/main/java/org/gongsquad/cozmowebbackend/CozmoWebBackendApplication.java
+++ cozmo-web-backend/src/main/java/org/gongsquad/cozmowebbackend/CozmoWebBackendApplication.java
@@ -1,4 +1,4 @@
-package gongsquad.cozmowebbackend;
+package org.gongsquad.cozmowebbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- cozmo-web-backend/src/test/java/org/gongsquad/cozmowebbackend/CozmoWebBackendApplicationTests.java
+++ cozmo-web-backend/src/test/java/org/gongsquad/cozmowebbackend/CozmoWebBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package gongsquad.cozmowebbackend;
+package org.gongsquad.cozmowebbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
