d75a4a1b7faa577938624e2d2e6267e38b25f7f9
cgganaban <gecocarlganaban@gmail.com>
2022-09-09 23:13:41 +0800

    init

---
+++ cozmo-web-backend/src/main/java/gongsquad/cozmowebbackend/CozmoWebBackendApplication.java
@@ -0,0 +1,13 @@
+package gongsquad.cozmowebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozmoWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozmoWebBackendApplication.class, args);
+	}
+
+}
